d1878f2cdeca5e67f44379c508037fbf3091a74b
angelica.merced <you@example.com>
2023-01-27 00:04:18 +0800

    add global variables

---
+++ global_var.py
@@ -0,0 +1,23 @@
+CELL_SIZE = 64
+EMPTY = 0
+WHITE = 1
+BLACK = 2
+MAXIMIZING_PLAYER = WHITE
+VALID_MOVE = 3
+LAST_MOVE = 4
+GAME_IN_PROGRESS = 0
+BLACK_WON = 2
+WHITE_WON = 3
+WHITE_TILE = "./assets/white_tile.png"
+BLACK_TILE = "./assets/black_tile.png"
+NEXT_MOVE_IMG = "./assets/next_move_tile.png"
+DIRECTIONS = [
+    "north",
+    "south",
+    "west",
+    "east",
+    "north_west",
+    "north_east",
+    "south_west",
+    "south_east"
+]
